4daffaacacc5446b365faea4d6e854d5e2c310f7
Abhiranjan0 <abhishekranjan7390@gmail.com>
2022-08-22 02:19:17 +0530

    first commit

---
+++ src/xyz/xyz.java
@@ -0,0 +1,14 @@
+package xyz;
+
+public class xyz {
+
+	public static void main(String[]args)
+	{  
+		System.out.println("The sum of:");
+	   int a=19,b=21;
+	   System.out.println(a);
+	   System.out.println(b);
+	   System.out.println(a+b);
+	}
+
+}
